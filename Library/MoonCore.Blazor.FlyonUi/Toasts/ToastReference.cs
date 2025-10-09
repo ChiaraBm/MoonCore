cb8b42fbfc22259ad5260a3b56653c0e267baafc
ChiaraBm <chiara.bm04@gmail.com>
2025-10-09 17:54:56 +0200

    Started creating an improved file upload handler and interop. Started redesigning toast handling and referencing

--- Library/MoonCore.Blazor.FlyonUi/Toasts/ToastReference.cs
+++ Library/MoonCore.Blazor.FlyonUi/Toasts/ToastReference.cs
@@ -1,11 +1,29 @@
-using Microsoft.AspNetCore.Components;
+using MoonCore.Blazor.FlyonUi.Toasts.Components;
 
 namespace MoonCore.Blazor.FlyonUi.Toasts;
 
-public class ToastReference
+public class ToastReference<T> : ToastReference where T : BaseToast
 {
-    /// <summary>
-    /// Content of the toast
-    /// </summary>
-    public RenderFragment Component { get; set; }
+    public ToastReference(Type componentType, Action<T>? configureAction)
+    {
+        ComponentType = componentType;
+
+        if (configureAction != null)
+            ConfigureAction = () => configureAction.Invoke(Instance);
+    }
+    
+    public T Instance { get; set; }
+
+    internal override BaseToast Component
+    {
+        get => Instance;
+        set => Instance = (T)value;
+    }
+}
+
+public abstract class ToastReference
+{
+    internal Type ComponentType { get; set; }
+    internal abstract BaseToast Component { get; set; }
+    internal Action? ConfigureAction { get; set; }
 }
